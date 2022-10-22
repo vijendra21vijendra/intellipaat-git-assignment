a6c7dcc0b2fba79b12df9d0fea3f2422dc27eeba
code <44273214+vijendra21vijendra@users.noreply.github.com>
2022-10-22 12:59:15 +0530

    commited changed code for jenkins assignment

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld{
  public static void main( String[] args){
-    System.out.println("Hello world from git-jenkins assignment"); 
-  System.out.println("this is done on 22nd Oct2022 for assignment of jenkins time: 12:35 pm "); 
+    System.out.println("this is done as jenkins project for intellipaat course on 22nd Oct, 12:59 pm");
  }
   
 }
