456879a56fdbf1cc2f7ea752d97a2af69b8baf12
code <44273214+vijendra21vijendra@users.noreply.github.com>
2022-10-22 11:13:53 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld{
  public static void main( String[] args){
     System.out.println("Hello world from git-jenkins assignment"); 
+  System.out.println("this is done on 22nd Oct2022 for assignment of jenkins  "); 
  }
   
 }
